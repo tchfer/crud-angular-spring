2b228d6bd5c727c974f927e1e44b0a11d33cec93
tchfer <tchfernando@gmail.com>
2021-11-02 15:36:01 -0300

    include error message dialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,29 +1,38 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Course } from './../model/course';
 import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  public courses$: Observable<Course[]>;
+  public displayedColumns = ['name', 'category'];
 
-  public courses$: Observable <Course[]>;
-  public displayedColumns = [
-    'name',
-    'category'
-  ];
-
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses = [] could be initialized here instead
-    this.courses$ = this.coursesService.list(); // is here now due to lack of initialization on declaration
-   }
-
-  ngOnInit(): void {
-
+    this.courses$ = this.coursesService.list().pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.');
+        return of([]);
+      })
+    ); // is here now due to lack of initialization on declaration
   }
 
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  ngOnInit(): void {}
 }
